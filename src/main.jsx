b9986a6e07a0bfd9d8f1f655fa311c7c049f82aa
Nahuelsand <rod.arena7@gmail.com>
2026-02-13 04:35:35 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // No importa si está vacío, App.jsx ya tiene sus estilos
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-3. Haz el `git add .`, `git commit -m "Solución pantalla blanca"` y `git push`.
-
