0a82fb88340d812a45f8404fc159f9f64757f4b5
Nahuelsand <rod.arena7@gmail.com>
2026-02-13 03:58:14 -0300

    Update main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+/* Estilos globales adicionales */
+body {
+  margin: 0;
+  background-color: #faf9f6;
+  -webkit-font-smoothing: antialiased;
+}
+
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
