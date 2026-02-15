ee4e3000a4a5bbfbed6bcfc54f1c31438b171c76
Nahuelsand <rod.arena7@gmail.com>
2026-02-15 05:55:24 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Aquí le decimos a Tailwind qué archivos observar
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      // Configuramos las fuentes para que coincidan con tu diseño elegante
       fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
         serif: ['Playfair Display', 'serif'],
-        sans: ['Inter', 'sans-serif'],
       },
     },
   },
