88390b6b38dfe1c1b18651d0c7272dff3e18767c
Nahuelsand <rod.arena7@gmail.com>
2026-04-17 21:19:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2646,10 +2646,8 @@ export default function App() {
         className="text-slate-500 hover:text-[#25D366] transition-colors p-2"
         title="WhatsApp"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347z"/>
-        </svg>
-      </a>
+  <img src="/whatsapp.png" alt="WhatsApp" className="w-6 h-6 object-contain" />
+        </a>
       {/* MercadoLibre */}
 <a
   href="https://www.mercadolibre.com.ar/pagina/outletmasroma#from=share_eshop"
@@ -2658,9 +2656,7 @@ export default function App() {
   className="text-slate-500 hover:text-[#FFE600] transition-colors p-2"
   title="MercadoLibre"
 >
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-13h-2v6h2V7zm0 8h-2v2h2v-2z"/>
-  </svg>
+<img src="/mercadolibre.png" alt="MercadoLibre" className="w-6 h-6 object-contain" />
 </a>
 
 {/* Google Maps */}
